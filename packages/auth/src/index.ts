2d980851d11f33454bf53316665e5d6808f4c669
Nur Zaman <nur-zaman@users.noreply.github.com>
2025-12-31 02:12:20 +0600

    init commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,42 @@
+import { tanstackStartCookies } from 'better-auth/tanstack-start';
+import { db } from "@base-template/db";
+import * as schema from "@base-template/db/schema/auth";
+import { env } from "@base-template/env/server";
+import { polar, checkout, portal } from "@polar-sh/better-auth";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+import { polarClient } from "./lib/payments";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      enableCustomerPortal: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "your-product-id",
+              slug: "pro",
+            },
+          ],
+          successUrl: env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+    tanstackStartCookies()
+  ],
+});
